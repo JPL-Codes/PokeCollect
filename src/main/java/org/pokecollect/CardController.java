6c9104339e1ea8d4b170b6e9152ee3b87f18d285
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2023-10-03 18:54:16 -0400

    wired controller to start.html template

--- src/main/java/org/pokecollect/CardController.java
+++ src/main/java/org/pokecollect/CardController.java
@@ -1,8 +1,18 @@
 package org.pokecollect;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 //testing spring annotations to ensure successful import
 @Controller
 public class CardController {
+
+    /**
+     * Listens for a connection to root (/) endpoint
+     * @return start page
+     */
+    @RequestMapping("/")
+    public String index() {
+        return "start";
+    }
 }
