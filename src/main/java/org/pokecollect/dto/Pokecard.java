fb0291265b5f65f517c10d5612f0d980812d42bc
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2023-10-03 19:25:09 -0400

    wired controller to start.html template and added workspace.xml to gitignore

---
+++ src/main/java/org/pokecollect/dto/Pokecard.java
@@ -0,0 +1,12 @@
+package org.pokecollect.dto;
+
+import java.util.List;
+
+public class Pokecard {
+
+    private int id;
+    private String name;
+    private String level;
+    private String hp;
+    private List<String> types;
+}
