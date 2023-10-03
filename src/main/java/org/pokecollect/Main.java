f25642d6e111f83b59f1b5f77a93bc242512e060
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2023-10-03 16:42:32 -0400

    changed package name from example to pokecollect and testing connection to my branch

--- src/main/java/org/pokecollect/Main.java
+++ src/main/java/org/pokecollect/Main.java
@@ -1,8 +1,10 @@
-package org.example;
+package org.pokecollect;
 
 public class Main {
     public static void main(String[] args) {
 
+
+        //testing git push to branch
         System.out.println("Hello world!12");
     }
 }
