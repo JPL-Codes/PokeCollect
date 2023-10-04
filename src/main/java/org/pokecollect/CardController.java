fe257d85985bbf73dc8c7506596a34cb2ceb2f5d
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2023-10-03 21:12:09 -0400

    mapped GET/POST/DELETE endpoints

--- src/main/java/org/pokecollect/CardController.java
+++ src/main/java/org/pokecollect/CardController.java
@@ -1,8 +1,10 @@
 package org.pokecollect;
 
 import org.pokecollect.dto.Pokecard;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 //testing spring annotations to ensure successful import
 @Controller
@@ -14,8 +16,26 @@ public class CardController {
      */
     @RequestMapping("/")
     public String index() {
-        Pokecard myFirstCard = new Pokecard();
-        myFirstCard.setName("Charmander");
         return "start";
+    }
+
+    @GetMapping("/pokecard")
+    public ResponseEntity getAllPokecards() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/pokecard/{id}/")
+    public ResponseEntity getPokecardById(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/pokecard", consumes = "application/json", produces = "application/json")
+    public Pokecard createPokecard(@RequestBody Pokecard card) {
+        return card;
+    }
+
+    @DeleteMapping("/pokecard/{id}/")
+    public ResponseEntity deletePokecard(@PathVariable("id") String id) {
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
