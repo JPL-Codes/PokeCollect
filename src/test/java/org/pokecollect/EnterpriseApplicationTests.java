4fa550bb3a9b4c81041b2028fcf03325f9972aa2
CrayarC52 <conleylp@mail.uc.edu>
2023-10-05 10:51:36 -0400

    Changes by john

--- src/test/java/org/pokecollect/EnterpriseApplicationTests.java
+++ src/test/java/org/pokecollect/EnterpriseApplicationTests.java
@@ -44,4 +44,33 @@ class EnterpriseApplicationTests {
         assertEquals(83, id);
     }
 
+    @Test
+    void fetchAllPokecards_returnAllPokecard() {
+        givenSearchFieldIsAvailable();
+        whenTheUserEntersCharizardAndClicksTheSearchButton();
+        thenReturnPokemonCardsNamedCharizard();
+    }
+
+    private void givenSearchFieldIsAvailable() {
+    }
+
+    private void whenTheUserEntersABlankSearch() {
+        pokecard = pokecardService.getAllPokecards();
+    }
+
+    private void thenReturnAllPokecards() {
+        ArrayList<Pokecard> myCollection = new ArrayList<>();
+        for (Pokecard card : pokecardList)
+        {
+            myCollection.add(card);
+        }
+        Pokecard card1 = new Pokecard();
+        card1.setName("Charmander");
+        Pokecard card2 = new Pokecard();
+        card1.setName("Squirtle");
+        Pokecard card3 = new Pokecard();
+        card1.setName("Pikachu");
+        assertArrayEquals(new ArrayList<Pokecard>[] = Arrays.asList(card1, card2, card3), myCollection.toArray());
+    }
+
 }
