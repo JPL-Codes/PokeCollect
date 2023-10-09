ff17440520e32cac27a4578f65c2332daf275e3d
JpLew <kingdimples2@yahoo.com>
2023-10-09 08:37:52 -0400

    Initial GUI

--- src/main/java/org/pokecollect/EnterpriseApplication.java
+++ src/main/java/org/pokecollect/EnterpriseApplication.java
@@ -1,12 +1,16 @@
 package org.pokecollect;
 
+import org.pokecollect.service.PokeFrame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class EnterpriseApplication {
 
     public static void main(String[] args) {
+        JFrame frame = new PokeFrame();
         SpringApplication.run(EnterpriseApplication.class, args);
     }
 
